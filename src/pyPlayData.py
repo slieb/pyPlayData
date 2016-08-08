d211d38499ed163fc46b0973b5ba35288f3a07dd
Steven Lieblich <steven.lieblich@gmail.com>
2016-08-08 17:29:18 -0400

    Added CSV and JSON file formats

--- src/pyPlayData.py
+++ src/pyPlayData.py
@@ -1,7 +1,24 @@
+import csv
+import json
 import xlrd
 import pprint
 
-book = xlrd.open_workbook ('../data/SOWC 2014 Stat Tables_Table 9.xlsx')
+# Opening a CSV file
+csvfile = open('../data/data-text.csv', 'rb')
+reader = csv.DictReader(csvfile)
+
+for row in reader:
+    print row
+
+# Opening a JSON file
+json_data = open('../data/data-text.json').read()
+data = json.loads(json_data)
+
+for item in data:
+    print item
+
+# Opening an EXCEL file
+book = xlrd.open_workbook('../data/SOWC 2014 Stat Tables_Table 9.xlsx')
 for sheet in book.sheets():
     print sheet.name
 
@@ -10,15 +27,14 @@ print "Number of rows is ", sheet.nrows
 
 count = 0
 data = {}
-
 for i in xrange(14, sheet.nrows):
     row = sheet.row_values(i)
     country = row[1]
 
     data[country] = {
         'child_labor': {
-            'total' : [row[4], row[5]],
-            'male' : [row[6], row[7]],
+            'total': [row[4], row[5]],
+            'male': [row[6], row[7]],
             'female': [row[8], row[9]],
         },
         'child_marraige': {
@@ -32,4 +48,4 @@ for i in xrange(14, sheet.nrows):
 print data['Afghanistan']
 
 print "Pretty Print:"
-pprint.pprint (data)
+pprint.pprint(data)
