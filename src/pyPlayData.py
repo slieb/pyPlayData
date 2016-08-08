a12a4dac11d1219ad8843045dfe82b394899bf38
Steven Lieblich <steven.lieblich@gmail.com>
2016-08-07 20:09:45 -0400

    Iterating through spreadsheet and extracting data

--- src/pyPlayData.py
+++ src/pyPlayData.py
@@ -1,4 +1,5 @@
 import xlrd
+import pprint
 
 book = xlrd.open_workbook ('../data/SOWC 2014 Stat Tables_Table 9.xlsx')
 for sheet in book.sheets():
@@ -7,5 +8,28 @@ for sheet in book.sheets():
 sheet = book.sheet_by_name('Table 9 ')
 print "Number of rows is ", sheet.nrows
 
-for i in range(sheet.nrows):
-    print sheet.row_values(i)
+count = 0
+data = {}
+
+for i in xrange(14, sheet.nrows):
+    row = sheet.row_values(i)
+    country = row[1]
+
+    data[country] = {
+        'child_labor': {
+            'total' : [row[4], row[5]],
+            'male' : [row[6], row[7]],
+            'female': [row[8], row[9]],
+        },
+        'child_marraige': {
+            'married_by_15': [row[10], row[11]],
+            'married_by_18': [row[12], row[13]],
+        }
+    }
+    if country == 'Zimbabwe':
+        break
+
+print data['Afghanistan']
+
+print "Pretty Print:"
+pprint.pprint (data)
